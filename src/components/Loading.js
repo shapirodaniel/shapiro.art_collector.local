12826961d824ff2b389026929f9361ca2950a534
shapirodaniel <shapirodanieladam@gmail.com>
2021-11-28 14:36:52 -0600

    add: loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 
-/**
- * Create and export a component called Loading which uses this static HTML template:
- * 
- * <div id="loading">
- *   <h2 className="message">Searching...</h2>
- * </div>
- */
+//  Create and export a component called Loading which uses this static HTML template:
+
+//  <div id="loading">
+//    <h2 className="message">Searching...</h2>
+//  </div>
+
+export default function Loading() {
+  return (
+    <div id="loading">
+      <h2 className="message">Searching...</h2>
+    </div>
+  );
+}
