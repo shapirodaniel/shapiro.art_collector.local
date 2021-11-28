9133e9134201cdccd6952df2ba14b20ec871eb4a
shapirodaniel <shapirodanieladam@gmail.com>
2021-11-28 14:36:00 -0600

    add: title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react";
 
-/**
- * Create/export a component called Title which uses this static HTML as the template:
- * 
- * <div id="title">
- *   <h1>
- *     The Art Collector
- *   </h1>
- *   <h5>
- *     Search the Harvard Art Museums' Private Collections
- *   </h5>
- * </div>
- */
+//  Create/export a component called Title which uses this static HTML as the template:
+
+//  <div id="title">
+//    <h1>
+//      The Art Collector
+//    </h1>
+//    <h5>
+//      Search the Harvard Art Museums' Private Collections
+//    </h5>
+//  </div>
+
+export default function Title() {
+  return (
+    <div id="title">
+      <h1>The Art Collector</h1>
+      <h5>Search the Harvard Art Museums' Private Collections</h5>
+    </div>
+  );
+}
