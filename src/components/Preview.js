d2a5e1fa4191fba65fbf76ab65791e8fd54acaa1
shapirodaniel <shapirodanieladam@gmail.com>
2021-11-28 14:48:57 -0600

    add: preview

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from "react";
 
 /**
  * We need to import fetchQueryResultsFromURL since we will sometimes have urls in info.prev and info.next
  * which are query urls.
  */
-import { fetchQueryResultsFromURL } from '../api';
+import { fetchQueryResultsFromURL } from "../api";
 
 const Preview = (props) => {
   /**
    * Destructure setSearchResults, setFeaturedResult, and setIsLoading from props
    * and also destructure info and records from props.searchResults
-   * 
+   *
    * You need info, records, setSearchResults, setFeaturedResult, and setIsLoading as available constants
    */
-
+  const { setSearchResults, setFeaturedResult, setIsLoading } = props;
+  const { info, records } = searchResults;
 
   /**
    * Don't touch this function, it's good to go.
-   * 
+   *
    * It has to be defined inside the Preview component to have access to setIsLoading, setSearchResults, etc...
    */
   async function fetchPage(pageUrl) {
@@ -33,22 +34,29 @@ const Preview = (props) => {
     }
   }
 
-  return <aside id="preview">
-    <header className="pagination">
-      {/* This button should be disabled if nothing is set in info.prev, and should call fetchPage with info.prev when clicked */}
-      <button 
-        disabled={} 
-        className="previous"
-        onClick={}>Previous</button>
-      {/* This button should be disabled if nothing is set in info.next, and should call fetchPage with info.next when clicked */}
-      <button
-        disabled={}
-        className="next"
-        onClick={}>Next</button>
-    </header>
-    <section className="results">
-      {
-        /* Here we should map over the records, and render something like this for each one:
+  return (
+    <aside id="preview">
+      <header className="pagination">
+        {/* This button should be disabled if nothing is set in info.prev, and should call fetchPage with info.prev when clicked */}
+        <button
+          disabled={!info.prev ? true : false}
+          className="previous"
+          onClick={() => fetchPage(info.prev)}
+        >
+          Previous
+        </button>
+        {/* This button should be disabled if nothing is set in info.next, and should call fetchPage with info.next when clicked */}
+        <button
+          disabled={!info.next ? true : false}
+          className="next"
+          onClick={() => fetchPage(info.next)}
+        >
+          Next
+        </button>
+      </header>
+      <section className="results">
+        {
+          /* Here we should map over the records, and render something like this for each one:
           <div  
             key={ index }
             className="object-preview"
@@ -64,9 +72,33 @@ const Preview = (props) => {
             }
           </div>
         */
-      }
-    </section>
-  </aside>
-}
+          records.map((record, index) => (
+            <div
+              key={index}
+              className="object-preview"
+              onClick={(event) => {
+                // prevent the default
+                // set the featured result to be this record, using setFeaturedResult
+                event.preventDefault();
+                setFeaturedResult(record);
+              }}
+            >
+              {
+                // if the record.primaryimageurl exists, show this: <img src={ record.primaryimageurl } alt={ record.description } />, otherwise show nothing
+                record.primaryimageurl && (
+                  <img src={record.primaryimageurl} alt={record.description} />
+                )
+              }
+              {
+                // if the record.title exists, add this: <h3>{ record.title }</h3>, otherwise show this: <h3>MISSING INFO</h3>
+                <h3>{record.title || "MISSING INFO"}</h3>
+              }
+            </div>
+          ))
+        }
+      </section>
+    </aside>
+  );
+};
 
 export default Preview;
