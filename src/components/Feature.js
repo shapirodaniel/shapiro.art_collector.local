6e7bbbbfeb46990836e2084a83fe81d10f22231d
shapirodaniel <shapirodanieladam@gmail.com>
2021-11-28 14:51:17 -0600

    add: searchable

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,45 +1,71 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
 // Don't touch this import
-import { fetchQueryResultsFromTermAndValue } from '../api';
+import { fetchQueryResultsFromTermAndValue } from "../api";
 
 /**
  * We need a new component called Searchable which:
- * 
+ *
  * Has a template like this:
- * 
+ *
  * <span className="content">
  *  <a href="#" onClick={async (event) => {}}>SOME SEARCH TERM</a>
  * </span>
  *
  * You'll need to read searchTerm, searchValue, setIsLoading, and setSearchResults off of the props.
- * 
+ *
  * When someone clicks the anchor tag, you should:
- * 
+ *
  * - preventDefault on the event
  * - call setIsLoading, set it to true
- * 
+ *
  * Then start a try/catch/finally block:
- * 
+ *
  * try:
  *  - await the result of fetchQueryResultsFromTermAndValue, passing in searchTerm and searchValue
  *  - send the result to setSearchResults (which will update the Preview component)
- * catch: 
+ * catch:
  *  - console.error the error
  * finally:
  *  - call setIsLoading, set it to false
  */
 const Searchable = (props) => {
-  
-}
+  const { searchTerm, searchValue, setIsLoading, setSearchResults } = props;
+
+  return (
+    <span className="content">
+      <a
+        href="#"
+        onClick={async (event) => {
+          event.preventDefault();
+          setIsLoading(true);
+
+          try {
+            const searchResults = await fetchQueryResultsFromTermAndValue(
+              searchTerm,
+              searchValue
+            );
+            setSearchResults(searchResults);
+          } catch (err) {
+            console.error(err);
+          } finally {
+            setIsLoading(false);
+          }
+        }}
+      >
+        {searchTerm}
+      </a>
+    </span>
+  );
+};
 
 /**
  * We need a new component called Feature which looks like this when no featuredResult is passed in as a prop:
- * 
+ *
  * <main id="feature"></main>
- * 
+ *
  * And like this when one is:
- * 
+ *
  * <main id="feature">
  *   <div className="object-feature">
  *     <header>
@@ -57,18 +83,16 @@ const Searchable = (props) => {
  *     </section>
  *   </div>
  * </main>
- * 
- * The different facts look like this: title, dated, images, primaryimageurl, description, culture, style, 
+ *
+ * The different facts look like this: title, dated, images, primaryimageurl, description, culture, style,
  * technique, medium, dimensions, people, department, division, contact, creditline
- * 
+ *
  * The <Searchable /> ones are: culture, technique, medium (first toLowerCase it), and person.displayname (one for each PEOPLE)
- * 
+ *
  * NOTE: people and images are likely to be arrays, and will need to be mapped over if they exist
- * 
+ *
  * This component should be exported as default.
  */
-const Feature = (props) => {
-
-}
+const Feature = (props) => {};
 
 export default Feature;
